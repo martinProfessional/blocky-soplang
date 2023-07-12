f69093c7cf584e05971c7a6c9369d407a55c2fab
Hyunjae Oh <moh36@wisc.edu>
2023-07-12 16:05:28 +0900

    commenting out the code form chatGpt

--- examples/blockly-react/src/fields/LoopField.jsx
+++ examples/blockly-react/src/fields/LoopField.jsx
@@ -1,47 +1,47 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-
-//import * as Blockly from 'blockly/core';
-
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 import * as Blockly from 'blockly/core';
 
-import BlocklyReactField from './BlocklyReactField';
+// import React from 'react';
+// import * as Blockly from 'blockly/core';
 
-class LoopField extends BlocklyReactField {
-  static fromJson(options) {
-    // Create an instance of LoopField with the specified loop count
-    return new this(options['count']);
-  }
+// import BlocklyReactField from './BlocklyReactField';
 
-  setCount(count) {
-    // Set the loop count and trigger a Blockly redraw
-    this.setValue(count);
-    Blockly.DropDownDiv.hideIfOwner(this, true);
-  }
+// class LoopField extends BlocklyReactField {
+//   static fromJson(options) {
+//     // Create an instance of LoopField with the specified loop count
+//     return new this(options['count']);
+//   }
 
-  getText_() {
-    // Get the loop count as a string
-    return String(this.value_);
-  }
+//   setCount(count) {
+//     // Set the loop count and trigger a Blockly redraw
+//     this.setValue(count);
+//     Blockly.DropDownDiv.hideIfOwner(this, true);
+//   }
 
-  fromXml(fieldElement) {
-    // Parse the loop count from XML and set it as the field value
-    this.setCount(parseInt(fieldElement.textContent, 10));
-  }
+//   getText_() {
+//     // Get the loop count as a string
+//     return String(this.value_);
+//   }
 
-  render() {
-    return (
-      <div>
-        <button onClick={() => this.setCount(this.value_ - 1)}>-</button>
-        <span>{this.getText_()}</span>
-        <button onClick={() => this.setCount(this.value_ + 1)}>+</button>
-      </div>
-    );
-  }
-}
+//   fromXml(fieldElement) {
+//     // Parse the loop count from XML and set it as the field value
+//     this.setCount(parseInt(fieldElement.textContent, 10));
+//   }
 
-Blockly.fieldRegistry.register('field_loop', LoopField);
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={() => this.setCount(this.value_ - 1)}>-</button>
+//         <span>{this.getText_()}</span>
+//         <button onClick={() => this.setCount(this.value_ + 1)}>+</button>
+//       </div>
+//     );
+//   }
+// }
 
-export default LoopField;
+// Blockly.fieldRegistry.register('field_loop', LoopField);
+
+// export default LoopField;
 
